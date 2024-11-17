af30451f23e18d058a3f436aae0198986bc271e2
Crytonical <brewster0503@gmail.com>
2024-11-16 23:26:49 -0500

    finished domomaker d

--- server/router.js
+++ server/router.js
@@ -2,10 +2,11 @@ const controllers = require('./controllers');
 const middleware = require('./middleware');
 
 const router = (app) => {
+  app.get('/getDomos', middleware.requiresLogin, controllers.Domo.getDomos);
+
   app.get('/login', middleware.requiresSecure, middleware.requiresLogout, controllers.Account.loginPage);
   app.post('/login', middleware.requiresSecure, middleware.requiresLogout, controllers.Account.login);
 
-  app.get('/signup', middleware.requiresSecure, middleware.requiresLogout, controllers.Account.signupPage);
   app.post('/signup', middleware.requiresSecure, middleware.requiresLogout, controllers.Account.signup);
 
   app.get('/logout', middleware.requiresLogin, controllers.Account.logout);
