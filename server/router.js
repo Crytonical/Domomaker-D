0a6c37d93955860d2babd5edcb9119ff6e3bfca1
Crytonical <brewster0503@gmail.com>
2024-11-16 20:17:47 -0500

    initial commit
     - copy pasted from domomaker a-c

---
+++ server/router.js
@@ -0,0 +1,19 @@
+const controllers = require('./controllers');
+const middleware = require('./middleware');
+
+const router = (app) => {
+  app.get('/login', middleware.requiresSecure, middleware.requiresLogout, controllers.Account.loginPage);
+  app.post('/login', middleware.requiresSecure, middleware.requiresLogout, controllers.Account.login);
+
+  app.get('/signup', middleware.requiresSecure, middleware.requiresLogout, controllers.Account.signupPage);
+  app.post('/signup', middleware.requiresSecure, middleware.requiresLogout, controllers.Account.signup);
+
+  app.get('/logout', middleware.requiresLogin, controllers.Account.logout);
+
+  app.get('/maker', middleware.requiresLogin, controllers.Domo.makerPage);
+  app.post('/maker', middleware.requiresLogin, controllers.Domo.makeDomo);
+
+  app.get('/', middleware.requiresSecure, middleware.requiresLogout, controllers.Account.loginPage);
+};
+
+module.exports = router;
